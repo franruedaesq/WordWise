027bd1334aa1e9f14a9dea31bcf2681630d6afa7
Francisco Rueda <70042618+franruedaesq@users.noreply.github.com>
2023-03-08 22:52:00 +0000

    feat: openAI integration - new interface

--- word-wise/src/pages/_document.tsx
+++ word-wise/src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { getInitColorSchemeScript } from '@mui/material/styles';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>Word Wise</Head>
+        <body>
+          {getInitColorSchemeScript()}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- word-wise/src/pages/index.tsx
+++ word-wise/src/pages/index.tsx
@@ -1,88 +1,173 @@
-import { IItem } from '@/utils/item';
-import { v4 as uuidv4 } from 'uuid';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography,
+} from '@mui/material';
+
+interface TextParams {
+  textType: string;
+  textSize: string;
+  textDifficulty: string;
+}
+
+interface Generatedtitle {
+  german: string;
+  english: string;
+}
+
+interface TextObj {
+  title: Generatedtitle;
+  text: string;
+}
+
+interface ResponseData {
+  response: {
+    role: string;
+    content: TextObj
+  };
+}
+
 
 const Home: React.FC = () => {
-  const [item, setItem] = useState<IItem>({
-    id: uuidv4(), // Genera un ID aleatorio
-    UserId: "",
-    Name: "",
-    Email: "",
-    Level: "",
-    TypeOfContent: "",
-    Progress: {
-      FlashCardsCompleted: 0,
-      QuizzesCompleted: 0,
-    },
+  const [selectedType, setSelectedType] = useState<string>('dialogue');
+  const [selectedSize, setSelectedSize] = useState<string>('medium');
+  const [selectedDifficulty, setSelectedDifficulty] = useState<string>('easy');
+  const [generatedText, setGeneratedText] = useState<string>('');
+  const [generatedTitle, setGeneratedTitle] = useState<Generatedtitle>({
+    german: "",
+    english: "",
   });
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const res = await fetch(`/api/dynamo/post`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        item: item,
-      }),
-    });
-    if (res.ok) {
-      setItem({
-        id: uuidv4(), // Genera un ID aleatorio
-        UserId: "",
-        Name: "",
-        Email: "",
-        Level: "",
-        TypeOfContent: "",
-        Progress: {
-          FlashCardsCompleted: 0,
-          QuizzesCompleted: 0,
-        },
-      });
-    }
+  const [newText, setNewText] = useState<string>("{}");
+
+  const handleGenerate = async () => {
+    // TODO: Implement ChatGPT API call to generate text
+    setGeneratedText("")
+    setGeneratedTitle({
+      german: "",
+      english: "",
+    })
+    fetchData(selectedType, selectedSize, selectedDifficulty)
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setItem({
-      ...item,
-      [event.target.name]: event.target.value,
-    });
+  const handleTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedType((event.target as HTMLInputElement).value);
   };
+
+  const handleSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedSize((event.target as HTMLInputElement).value);
+  };
+
+  const handleDifficultyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedDifficulty((event.target as HTMLInputElement).value);
+  };
+
+  async function fetchData(textType: string, textSize: string, textDifficulty: string) {
+    const params: TextParams = {
+      textType,
+      textSize,
+      textDifficulty,
+    };
+
+    const resp = await fetch('/api/openAI/getText', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(params)
+    });
+
+    const data = await resp.json()
+    console.log(data.response.content)
+    setNewText(data.response.content)
+  }
+
+  useEffect(() => {
+    try {
+      const textContent: TextObj = JSON.parse(newText)
+      console.log(textContent)
+      setGeneratedText(textContent.text)
+      setGeneratedTitle(textContent.title)
+    } catch (error) {
+      console.log(error)
+    }
+  }, [newText]);
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          User ID:
-          <input type="text" name="UserId" value={item.UserId} onChange={handleChange} />
-        </label>
-        <label>
-          Name:
-          <input type="text" name="Name" value={item.Name} onChange={handleChange} />
-        </label>
-        <label>
-          Email:
-          <input type="text" name="Email" value={item.Email} onChange={handleChange} />
-        </label>
-        <label>
-          Level:
-          <input type="text" name="Level" value={item.Level} onChange={handleChange} />
-        </label>
-        <label>
-          Type of Content:
-          <input type="text" name="TypeOfContent" value={item.TypeOfContent} onChange={handleChange} />
-        </label>
-        <label>
-          Flash Cards Completed:
-          <input type="number" name="FlashCardsCompleted" value={item.Progress.FlashCardsCompleted} onChange={e => setItem({ ...item, Progress: { ...item.Progress, FlashCardsCompleted: parseInt(e.target.value) } })} />
-        </label>
-        <label>
-          Quizzes Completed:
-          <input type="number" name="QuizzesCompleted" value={item.Progress.QuizzesCompleted} onChange={e => setItem({ ...item, Progress: { ...item.Progress, QuizzesCompleted: parseInt(e.target.value) } })} />
-        </label>
-        <button type="submit">Add Item</button>
-      </form>
+      <Box display='flex' flexDirection='column' alignItems="center" justifyContent="space-evenly" height="300px">
+      <Typography variant="h4" component="h1" gutterBottom>
+        Generate German Text
+      </Typography>
+      <Box display='flex'>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Type of Text</FormLabel>
+        <RadioGroup
+          aria-label="text-type"
+          name="text-type"
+          value={selectedType}
+          onChange={handleTypeChange}
+        >
+          <FormControlLabel value="dialogue" control={<Radio />} label="Dialogue" />
+          <FormControlLabel value="story" control={<Radio />} label="Story" />
+          <FormControlLabel value="data" control={<Radio />} label="Random Data" />
+        </RadioGroup>
+      </FormControl>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Size of Text</FormLabel>
+        <RadioGroup
+          aria-label="text-size"
+          name="text-size"
+          value={selectedSize}
+          onChange={handleSizeChange}
+        >
+          <FormControlLabel value="short" control={<Radio />} label="Short" />
+          <FormControlLabel value="medium" control={<Radio />} label="Medium" />
+          <FormControlLabel value="large" control={<Radio />} label="Large" />
+        </RadioGroup>
+      </FormControl>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Difficulty</FormLabel>
+        <RadioGroup
+          aria-label="text-difficulty"
+          name="text-difficulty"
+          value={selectedDifficulty}
+          onChange={handleDifficultyChange}
+        >
+          <FormControlLabel value="easy" control={<Radio />} label="Easy" />
+          <FormControlLabel value="medium" control={<Radio />} label="Medium" />
+          <FormControlLabel value="expert" control={<Radio />} label="Expert" />
+        </RadioGroup>
+      </FormControl>
+      </Box>
+      <Button variant="contained" color="primary" onClick={handleGenerate} size="small" sx={{ maxWidth: "250px"}}>
+        Generate
+      </Button>
+      </Box>
+
+      <Box display="flex" flexDirection="column" alignItems="center">
+      <Box display="flex" mt={4} ml={3}>
+        <Typography variant="h5" component="h4" gutterBottom mr={1}>
+          {generatedTitle?.german}
+        </Typography> 
+        <Typography variant="h5" component="h5" gutterBottom>
+           {generatedTitle?.english}
+        </Typography>
+      </Box>
+      {/* <Typography variant="h6" component="h4" gutterBottom>
+        {generatedTitle?.german} - {generatedTitle?.english}
+      </Typography> */}
+      <Typography variant="body1" component="p" gutterBottom whiteSpace="pre-line" mt={1} ml={3}>
+          {generatedText && generatedText}
+      </Typography> 
+      </Box>
     </div>
   );
 };
